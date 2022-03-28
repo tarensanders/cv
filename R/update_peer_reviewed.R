581464bb731f482132ab9aefc2d76d2ab5adfe37
Taren Sanders <taren.sanders@acu.edu.au>
2022-03-28 22:16:13 +1100

    Create short version of CV

--- R/update_peer_reviewed.R
+++ R/update_peer_reviewed.R
@@ -5,24 +5,28 @@
 #' @title
 #' @param peer_reviewed
 #' @param incites_data
-update_peer_reviewed <- function(peer_reviewed, incites_data) {
+#' @param short_pubs
+update_peer_reviewed <- function(peer_reviewed, incites_data, short_pubs) {
   updated_peer_reviewed <-
     peer_reviewed %>%
     left_join(incites_data, by = c("annote" = "ut")) %>%
-    mutate(annote = case_when(
-      tot_cites > 0 & hot_paper ~
-      glue::glue(
-        "\\\ | Citations: {tot_cites}; JIF: {round(impact_factor,1)}; CNCI: {round(nci,1)}; WoS Hot Paper" # nolint
-      ),
-      tot_cites > 0 & !hot_paper ~
-      glue::glue(
-        "\\\ | Citations: {tot_cites}; JIF: {round(impact_factor,1)}; CNCI: {round(nci,1)}" # nolint
-      ),
-      !is.na(impact_factor) ~
-      glue::glue("\\\ | JIF: {round(impact_factor,1)}"),
-      TRUE ~ NA_character_
-    )) %>%
-    select(DOI:issue)
+    mutate(
+      short_cv = annote %in% short_pubs,
+      annote = case_when(
+        tot_cites > 0 & hot_paper ~
+        glue::glue(
+          "\\\ | Citations: {tot_cites}; JIF: {round(impact_factor,1)}; CNCI: {round(nci,1)}; WoS Hot Paper" # nolint
+        ),
+        tot_cites > 0 & !hot_paper ~
+        glue::glue(
+          "\\\ | Citations: {tot_cites}; JIF: {round(impact_factor,1)}; CNCI: {round(nci,1)}" # nolint
+        ),
+        !is.na(impact_factor) ~
+        glue::glue("\\\ | JIF: {round(impact_factor,1)}"),
+        TRUE ~ NA_character_
+      )
+    ) %>%
+    select(DOI:issue, short_cv)
 
   return(updated_peer_reviewed)
 }
--- _targets.R
+++ _targets.R
@@ -9,36 +9,41 @@ googledrive::drive_deauth()
 
 data_sheet <- "1eglf3B_N1yv-RH-48NBzriIslqSLkh30V2kxbxlWCfE"
 
+# Pubs to include in short CV
+short_pubs <- c(
+  "WOS:000620749700001",
+  "WOS:000712220800001",
+  "WOS:000648645400006",
+  "WOS:000627077400001",
+  "WOS:000667241100005",
+  "WOS:000660894000001",
+  "WOS:000530217600006",
+  "WOS:000501313400002",
+  "WOS:000491218300022",
+  "WOS:000361938100001"
+)
+
 tar_plan(
-  # Local files
+  # Bib files
   tar_target(
     peer_reviewed_file,
-    "cv/Peer-reviewed.bib",
+    "cv/bibs/Peer-reviewed.bib",
     format = "file"
   ),
   tar_target(
     book_chapters_file,
-    "cv/Book Chapters.bib",
+    "cv/bibs/Book Chapters.bib",
     format = "file"
   ),
   tar_target(
     conferences_file,
-    "cv/Conference Presentations.bib",
+    "cv/bibs/Conference Presentations.bib",
     format = "file"
-  ),
-  # tar_target(,
-  # peer_reviewed_file,
-  # "cv/My Library.bib",
-  # format = "file"),
-  # Processing
-  tar_target(
-    modified_date,
-    get_date_modified(data_sheet),
-    cue = tar_cue(mode = "always")
   ),
   tar_target(peer_reviewed, bibliography_entries(peer_reviewed_file)),
   tar_target(book_chapters, bibliography_entries(book_chapters_file)),
   tar_target(conferences, bibliography_entries(conferences_file)),
+  # Profiles
   tar_target(
     gscholar_profile,
     scholar::get_profile("8KNzhS4AAAAJ"),
@@ -51,13 +56,20 @@ tar_plan(
       name = incites_data, age = as.difftime(7, units = "days")
     )
   ),
+  tar_target(short_cv_pubs, short_pubs),
   tar_target(
     peer_reviewed_citations,
-    update_peer_reviewed(peer_reviewed, incites_data)
+    update_peer_reviewed(peer_reviewed, incites_data, short_cv_pubs)
   ),
   tar_target(
     research_profile,
     make_profile(gscholar_profile, incites_data, peer_reviewed_citations)
+  ),
+  # Google Sheets Files
+  tar_target(
+    modified_date,
+    get_date_modified(data_sheet),
+    cue = tar_cue(mode = "always")
   ),
   tar_target(funding, get_sheet(data_sheet, "Funding", modified_date)),
   tar_target(students, get_sheet(data_sheet, "Students", modified_date)),
@@ -75,5 +87,12 @@ tar_plan(
     invited_talks,
     get_sheet(data_sheet, "InvitedTalks", modified_date)
   ),
-  tar_render(cv, "./cv/cv.Rmd")
+  tar_render(cv, here::here("cv", "cv.Rmd"),
+    params = list(long = TRUE),
+    output_file = here::here("cv", "cv.pdf")
+  ),
+  tar_render(cv_short, here::here("cv", "cv.Rmd"),
+    params = list(long = FALSE),
+    output_file = here::here("cv", "cv_short.pdf")
+  )
 )
