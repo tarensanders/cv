7c29961983ab598df2d74ff990d3979dd98e11fa
Taren Sanders <taren.sanders@acu.edu.au>
2022-06-22 08:09:05 +1000

    Always update scholar citations

--- _targets.R
+++ _targets.R
@@ -44,10 +44,10 @@ tar_plan(
   tar_target(book_chapters, bibliography_entries(book_chapters_file)),
   tar_target(conferences, bibliography_entries(conferences_file)),
   # Profiles
-  tar_age(
+  tar_target(
     gscholar_profile,
     scholar::get_profile("8KNzhS4AAAAJ"),
-    age = as.difftime(7, units = "days")
+    cue = tar_cue(mode = "always")
   ),
   tar_age(
     incites_data,
