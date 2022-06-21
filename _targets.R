322a74a94df5d48ff2c1a9283fd64e337c35e355
Taren Sanders <taren.sanders@acu.edu.au>
2022-06-22 08:04:52 +1000

    Force citations to update every week

--- _targets.R
+++ _targets.R
@@ -44,13 +44,15 @@ tar_plan(
   tar_target(book_chapters, bibliography_entries(book_chapters_file)),
   tar_target(conferences, bibliography_entries(conferences_file)),
   # Profiles
-  tar_target(
+  tar_age(
     gscholar_profile,
-    scholar::get_profile("8KNzhS4AAAAJ")
+    scholar::get_profile("8KNzhS4AAAAJ"),
+    age = as.difftime(7, units = "days")
   ),
-  tar_target(
+  tar_age(
     incites_data,
-    get_incites_data(peer_reviewed)
+    get_incites_data(peer_reviewed),
+    age = as.difftime(7, units = "days")
   ),
   tar_target(short_cv_pubs, short_pubs),
   tar_target(
